8c5b46461c35e335397ba6db8f619f3c9345e0d0
abusayedparvez <abusayedparvez.pharmacy.pust@gmail.com>
2026-02-01 05:24:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -92,3 +92,11 @@ function type(){
 }
 
 type();
+
+/* mobile menu toggle */
+const toggle = document.querySelector(".nav__toggle");
+const menu = document.querySelector(".nav__menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
